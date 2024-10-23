376dcd803911e59f02b8652679a74e5ed698bf88
Lee Hyungi <geist9110@naver.com>
2024-10-23 15:12:38 +0900

    feat: 백준 25314번 문제
    https://www.acmicpc.net/problem/25314

---
+++ src/test/java/baekjoon/Problem_25314Test.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import baekjoon.config.ProblemTestConfigure;
+import java.io.IOException;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Problem_25314Test extends ProblemTestConfigure {
+
+    @ParameterizedTest
+    @MethodSource("testCase")
+    protected void test(String input, String actual) throws IOException {
+        run(input, actual, new Problem_25314());
+    }
+
+    private static Stream<Arguments> testCase() {
+        return Stream.of(
+            Arguments.of("4", "long int"),
+            Arguments.of("20", "long long long long long int"),
+            Arguments.of("15", "long long long long int"),
+            Arguments.of("100", "long long long long long long long long long long long long long long long long long long long long long long long long long int")
+        );
+    }
+}
