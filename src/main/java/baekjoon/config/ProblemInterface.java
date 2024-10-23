7fc338d640ab1ed5f9db8b0d39c948c9145e34dd
Lee Hyungi <geist9110@naver.com>
2024-10-23 14:40:14 +0900

    feat: 문제의 테스트 코드 작성을 위한 설정 추가

---
+++ src/main/java/baekjoon/config/ProblemInterface.java
@@ -0,0 +1,8 @@
+package baekjoon.config;
+
+import java.io.IOException;
+
+public interface ProblemInterface {
+
+    void solution(String[] args) throws IOException;
+}
---
+++ src/test/java/baekjoon/config/ProblemTestConfigure.java
@@ -0,0 +1,42 @@
+package baekjoon.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class ProblemTestConfigure {
+
+    protected abstract void test(String input, String actual) throws IOException;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    protected final void run(String input, String actual, ProblemInterface problemInterface)
+        throws IOException {
+        setInput(input);
+        problemInterface.solution(null);
+        assertEquals(outContent.toString(), actual);
+    }
+
+    private void setInput(String input) {
+        ByteArrayInputStream inContent = new ByteArrayInputStream(
+            input.getBytes(StandardCharsets.UTF_8));
+        System.setIn(inContent);
+    }
+}
