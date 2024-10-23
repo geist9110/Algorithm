15248e36bbe72590dd706198ee0aade4b0fd35af
Lee Hyungi <geist9110@naver.com>
2024-10-24 07:22:49 +0900

    feat: 백준 10810번 문제
    https://www.acmicpc.net/problem/10810

--- src/test/java/baekjoon/Problem_25314_Test.java
+++ src/test/java/baekjoon/Problem_25314_Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class Problem_25314Test extends ProblemTestConfigure {
+class Problem_25314_Test extends ProblemTestConfigure {
 
     @ParameterizedTest
     @MethodSource("testCase")
