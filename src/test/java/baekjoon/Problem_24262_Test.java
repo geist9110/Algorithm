6cb6933d8b7d431c3040e43a662dd7d6fb973820
Lee Hyungi <geist9110@naver.com>
2024-11-16 16:47:50 +0900

    feat: 백준 24262번 문제
    https://www.acmicpc.net/problem/24262

---
+++ src/test/java/baekjoon/Problem_24262_Test.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import baekjoon.config.ProblemTestConfigure;
+import java.io.IOException;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Problem_24262_Test extends ProblemTestConfigure {
+
+    @ParameterizedTest
+    @MethodSource("testCase")
+    protected void test(String input, String actual) throws IOException {
+        run(input, actual, new Problem_24262());
+    }
+
+    private static Stream<Arguments> testCase() {
+        return Stream.of(
+            Arguments.of("1", "1\n0")
+        );
+    }
+}
