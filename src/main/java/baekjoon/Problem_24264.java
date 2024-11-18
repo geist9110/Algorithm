c9b009febc78c5e797d2d90f5237ff57e0aa7b2d
Lee Hyungi <geist9110@naver.com>
2024-11-18 11:04:54 +0900

    feat: 백준 24264번 문제
    https://www.acmicpc.net/problem/24264

---
+++ src/main/java/baekjoon/Problem_24264.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import baekjoon.config.ProblemInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_24264 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.print(n * n + "\n2");
+    }
+}
---
+++ src/test/java/baekjoon/Problem_24264_Test.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import baekjoon.config.ProblemTestConfigure;
+import java.io.IOException;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Problem_24264_Test extends ProblemTestConfigure {
+
+    @ParameterizedTest
+    @MethodSource("testCase")
+    protected void test(String input, String actual) throws IOException {
+        run(input, actual, new Problem_24264());
+    }
+
+    private static Stream<Arguments> testCase() {
+        return Stream.of(
+            Arguments.of("7", "49\n2")
+        );
+    }
+}
