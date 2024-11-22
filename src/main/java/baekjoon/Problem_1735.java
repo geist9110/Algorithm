244bc54f28cfedd111a1f5f9ab66958f9be5acef
Lee Hyungi <geist9110@naver.com>
2024-11-23 07:21:36 +0900

    feat: 백준 1735번 문제
    https://www.acmicpc.net/problem/1735

---
+++ src/main/java/baekjoon/Problem_1735.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import baekjoon.config.ProblemInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_1735 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int denominator = b * d;
+        int numerator = a * d + c * b;
+        int gcd = gcd(numerator, denominator);
+        System.out.printf("%d %d", numerator / gcd, denominator / gcd);
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
