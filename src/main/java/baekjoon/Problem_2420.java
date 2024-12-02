fde5906c11dd42153d4218edc37b47239871ba01
geist9110 <geist9110@naver.com>
2024-12-03 08:59:39 +0900

    feat: 백준 2420번 문제
    https://www.acmicpc.net/problem/2420

---
+++ src/main/java/baekjoon/Problem_2420.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import baekjoon.config.ProblemInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_2420 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.print(
+            Math.abs(Long.parseLong(st.nextToken()) - Long.parseLong(st.nextToken()))
+        );
+    }
+}
