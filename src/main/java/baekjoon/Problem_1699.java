591ccba679ccbfeffbf4b5bf4276b58402682c01
geist9110 <geist9110@naver.com>
2025-02-14 14:01:37 +0900

    feat: 백준 1699번 문제
    https://www.acmicpc.net/problem/1699

---
+++ src/main/java/baekjoon/Problem_1699.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import baekjoon.config.ProblemInterface;
+
+public class Problem_1699 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = i;
+            for (int j = 1; j <= (int) Math.sqrt(i); j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
