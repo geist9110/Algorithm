79850419cbfc315b3ec5f176f6e8c1859b6001b6
geist9110 <geist9110@naver.com>
2025-03-30 11:03:16 +0900

    feat: 백준 2163번 문제
    https://www.acmicpc.net/problem/2163

---
+++ src/main/java/baekjoon/Problem_2163.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import baekjoon.config.ProblemInterface;
+
+public class Problem_2163 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.print(Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()) - 1);
+    }
+
+}
