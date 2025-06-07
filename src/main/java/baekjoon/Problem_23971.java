574eb03263a0667f5e43ee1524ecb7137ac7c9fe
geist9110 <geist9110@naver.com>
2025-06-07 09:42:19 +0900

    feat: 백준 23971번 문제
    https://www.acmicpc.net/problem/23971

---
+++ src/main/java/baekjoon/Problem_23971.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import baekjoon.config.ProblemInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_23971 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.print(((H + N) / (N + 1)) * ((W + M) / (M + 1)));
+    }
+}
---
+++ src/test/java/baekjoon/Problem_23971_Test.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import baekjoon.config.ProblemTestConfigure;
+import java.io.IOException;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Problem_23971_Test extends ProblemTestConfigure {
+
+    @ParameterizedTest
+    @MethodSource("testCase")
+    protected void test(String input, String actual) throws IOException {
+        run(input, actual, new Problem_23971());
+    }
+
+    private static Stream<Arguments> testCase() {
+        return Stream.of(
+            Arguments.of("5 4 1 1", "6")
+        );
+    }
+}
