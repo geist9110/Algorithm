0cef1d067fb983e55d94a8a5bf172e718d14101f
geist9110 <geist9110@naver.com>
2025-06-09 10:40:35 +0900

    feat: 백준 2442번 문제
    https://www.acmicpc.net/problem/2442

---
+++ src/main/java/baekjoon/Problem_2442.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import baekjoon.config.ProblemInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2442 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
