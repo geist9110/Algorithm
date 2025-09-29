136f978535d7a323276ff4813bf25de07098ea35
geist9110 <geist9110@naver.com>
2025-09-29 10:20:09 +0900

    feat: 백준 11867번 문제
    https://www.acmicpc.net/problem/11867

---
+++ src/main/java/baekjoon/Problem_11867.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import baekjoon.config.ProblemInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_11867 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (N % 2 == 1 && M % 2 == 1) {
+            System.out.print("B");
+            return;
+        }
+
+        System.out.print("A");
+    }
+}
