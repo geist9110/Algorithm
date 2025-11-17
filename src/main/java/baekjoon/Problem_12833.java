e19502e8dc6f7896b85cc2a2ae7dc6a82cd4649d
Lee Hyungi <geist9110@naver.com>
2025-11-18 08:23:13 +0900

    feat: 백준 12833번 문제
    https://www.acmicpc.net/problem/12833

---
+++ src/main/java/baekjoon/Problem_12833.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import baekjoon.config.ProblemInterface;
+
+public class Problem_12833 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < C; i++) {
+            A = A ^ B;
+        }
+
+        System.out.print(A);
+    }
+
+}
