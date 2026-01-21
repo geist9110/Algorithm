3e3e83a171064d31bbd0cc904a66d7196de23e9a
Lee Hyungi <geist9110@naver.com>
2026-01-22 07:46:38 +0900

    feat: 백준 23560번 문제
    https://www.acmicpc.net/problem/23560

---
+++ src/main/java/baekjoon/Problem_23560.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import baekjoon.config.ProblemInterface;
+
+public class Problem_23560 implements ProblemInterface {
+
+    public void solution(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(2 * (int) Math.pow(3, n - 1));
+    }
+}
---
+++ src/test/java/baekjoon/Problem_23560_Test.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import baekjoon.config.ProblemTestConfigure;
+
+public class Problem_23560_Test extends ProblemTestConfigure {
+
+    @ParameterizedTest
+    @MethodSource("testCase")
+    protected void test(String input, String actual) throws IOException {
+        run(input, actual, new Problem_23560());
+    }
+
+    private static Stream<Arguments> testCase() {
+        return Stream.of(
+                Arguments.of("1", "2"),
+                Arguments.of("2", "6"));
+    }
+
+}
